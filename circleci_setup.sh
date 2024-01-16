ced58827e34f9e6e45e9e2a09c53b44b632eda03
Devesh Chaudhari <DeveshRx@users.noreply.github.com>
2024-01-16 13:17:18 +0530

    Update circleci_setup.sh

--- circleci_setup.sh
+++ circleci_setup.sh
@@ -9,5 +9,10 @@ echo $SIGN_KEY_BASE64 > key.b64
 base64 -d key.b64 >> key.jks
 
 # Generate google-services.json file
-echo $GOOGLE_SERVICE_JSON_DEBUG_BASE64 > gsjd.b64
+echo $GOOGLE_SERVICE_JSON_BASE64 > gsjd.b64
 base64 -d gsjd.b64 >> /home/circleci/project/app/google-services.json
+
+
+# Generate GOOGLE_APPLICATION_CREDENTIALS file
+echo $GOOGLE_APPLICATION_CREDENTIALS_BASE64 > gac.b64
+base64 -d gac.b64 >> /home/circleci/project/GOOGLE_APPLICATION_CREDENTIALS.json
